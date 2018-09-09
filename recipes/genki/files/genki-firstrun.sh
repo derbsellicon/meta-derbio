caa3ffcc18b8d7103d149813bb787349a1e33ba4
Zakaria ELQotbi <zakaria@gen.ki>
2018-09-09 22:49:32 +0000

    append to firstrun

--- recipes/genki/files/genki-firstrun.sh
+++ recipes/genki/files/genki-firstrun.sh
@@ -5,7 +5,7 @@ i2cset -y 0 0x4b 0x30
 
 # 2. fix the mac address of eth0
 macaddr=$(cat /sys/class/net/eth0/address)
-echo <<EOF > /etc/systemd/network/20-eth0-mac.link 
+cat <<EOF > /etc/systemd/network/20-eth0-mac.link
 [Match]
 Driver=smsc75xx
 
