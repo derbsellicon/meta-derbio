5b0c2183414a274f2ff9d5cacb260552d8358ae9
Zakaria ELQotbi <zakaria@gen.ki>
2018-09-16 23:32:29 +0000

    dont unblock after chrome lunch

--- recipes/genki/files/genki-kiosk.sh
+++ recipes/genki/files/genki-kiosk.sh
@@ -6,8 +6,9 @@ URL="file:///opt/genki/data/genki.mhtml"
 export XDG_RUNTIME_DIR=/run/user/1000
 sleep 5
 #LayerManagerControl get screen 0
-sleep 5
-google-chrome --disable-session-crashed-bubble --kiosk --disable-infobars $URL &
+#sleep 5
+#google-chrome --disable-session-crashed-bubble --kiosk --disable-infobars $URL &
+google-chrome --disable-session-crashed-bubble --kiosk --disable-infobars $URL
 sleep 5
 #layer-add-surfaces 0 1000 2
 #LayerManagerControl set surface 7001 destination region 0 0 1920 1080
