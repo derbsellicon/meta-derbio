96058364ad86eef073db9def49456818c5e34ce1
Zakaria ELQotbi <zakaria@gen.ki>
2018-09-16 23:31:04 +0000

    generate also mac address for ethernet
    and make the script start really at the end of the boot

--- recipes/genki/files/genki-firstrun.sh
+++ recipes/genki/files/genki-firstrun.sh
@@ -4,14 +4,24 @@
 i2cset -y 0 0x4b 0x30
 
 # 2. fix the mac address of eth0
-macaddr=$(cat /sys/class/net/eth0/address)
+macaddre=$(cat /sys/class/net/eth0/address)
 cat <<EOF > /etc/systemd/network/20-eth0-mac.link
 [Match]
 Driver=smsc75xx
 
 [Link]
 Name=eth0
-MACAddress=${macaddr}
+MACAddress=${macaddre}
+EOF
+
+macaddrw=$(cat /sys/class/net/wlan0/address)
+cat <<EOF > /etc/systemd/network/21-wlan0-mac.link
+[Match]
+Driver=wcn36xx
+
+[Link]
+Name=wlan0
+MACAddress=${macaddrw}
 EOF
 
 
